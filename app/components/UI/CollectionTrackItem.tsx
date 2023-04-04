0bda8285c213721e656df019b195b7846520ab4b
VLADISLAW9 <80681837+VLADISLAW9@users.noreply.github.com>
2023-04-05 01:01:06 +0700

    add tracks block on Collection

---
+++ app/components/UI/CollectionTrackItem.tsx
@@ -0,0 +1,128 @@
+import { useActions } from '@/app/hooks/actions.hook'
+import { useAppSelector } from '@/app/hooks/selector.hook'
+import { IMusic } from '@/app/types/IMusic'
+import { FC, useEffect, useState } from 'react'
+import { BsFillHeartFill, BsPauseFill, BsPlayFill } from 'react-icons/bs'
+
+interface CollectionTrackProps {
+	mus: IMusic
+	index: number
+}
+
+const CollectionTrackItem: FC<CollectionTrackProps> = ({ mus, index }) => {
+	const [isHover, setIsHover] = useState(false)
+	const { activeTrack, stateTrack } = useAppSelector(state => state.player)
+	const [isPlay, setIsPlay] = useState(false)
+	const { setActiveTrack, playTrack, pauseTrack } = useActions()
+
+	useEffect(() => {
+		if (activeTrack) {
+			if (mus.name === activeTrack.name) {
+				if (!stateTrack) {
+					setIsPlay(false)
+				} else {
+					setIsPlay(true)
+				}
+			} else {
+				setIsPlay(false)
+			}
+		} else {
+			setIsPlay(false)
+		}
+	}, [activeTrack, stateTrack])
+
+	const play = (e: any) => {
+		e.stopPropagation()
+		if (activeTrack) {
+			if (mus.name === activeTrack.name) {
+				playTrack()
+				setIsPlay(true)
+			} else setActiveTrack(mus)
+		} else {
+			playTrack()
+			setIsPlay(true)
+			setActiveTrack(mus)
+		}
+	}
+
+	const stop = (e: any) => {
+		e.stopPropagation()
+		pauseTrack()
+		setIsPlay(false)
+	}
+
+	return (
+		<li
+			onMouseEnter={() => {
+				setIsHover(true)
+			}}
+			onMouseLeave={() => {
+				setIsHover(false)
+			}}
+			className='relative flex p-2 cursor-pointer border-2 cursor-pointer border-[#181818] hover:border-[#FFCC00]'
+		>
+			<div
+				className={
+					isHover
+						? 'opacity-0 d-track__start-column text-white/50 font-light'
+						: 'd-track__start-column text-white/50 font-light'
+				}
+			>
+				{index + 1}
+			</div>
+			{isHover && (
+				<>
+					{!isPlay ? (
+						<button
+							onClick={play}
+							className='absolute bg-[#FFCC00] rounded-full p-[5px] top-[7px] left-[4px] opacity-80 hover:opacity-100 transition-opacity'
+						>
+							<BsPlayFill
+								className='translate-x-px
+							'
+							/>
+						</button>
+					) : (
+						<button
+							onClick={stop}
+							className='absolute bg-[#FFCC00] rounded-full p-[5px] top-[7px] left-[4px] opacity-80 hover:opacity-100 transition-opacity'
+						>
+							<BsPauseFill
+								className='
+							'
+							/>
+						</button>
+					)}
+				</>
+			)}
+			<div className='d-track__name text-white'>{mus.name}</div>
+			<div className='d-track__artist'>
+				<div className='flex'>
+					<div className='text-white d-track__name'>{mus.author}</div>
+					<div className='d-track__moreinfo'>
+						<button>
+							<BsFillHeartFill
+								className={
+									isHover
+										? 'translate-y-1 w-5 h-5 text-[#FFCC00]'
+										: 'translate-y-1 w-5 h-5 text-white/50'
+								}
+							/>
+						</button>
+					</div>
+					<div
+						className={
+							isHover
+								? 'text-white opacity-0 font-light'
+								: 'text-white/50 font-light'
+						}
+					>
+						1:40
+					</div>
+				</div>
+			</div>
+		</li>
+	)
+}
+
+export default CollectionTrackItem
--- app/components/screens/collection/tracks/Tracks.tsx
+++ app/components/screens/collection/tracks/Tracks.tsx
@@ -1,7 +1,30 @@
+import CollectionTrackItem from '@/app/components/UI/CollectionTrackItem'
+import { useAppSelector } from '@/app/hooks/selector.hook'
 import { FC } from 'react'
 
 const Tracks: FC = () => {
-	return <div>Tracks</div>
+	const { favorites } = useAppSelector(state => state.favoritesSlice)
+
+	return (
+		<div>
+			<h1
+				className='text-white text-xl 
+			font-semibold'
+			>
+				Tracks
+			</h1>
+			<ul className='mt-5'>
+				<li className='flex ml-[10px] mb-5'>
+					<div className='text-white/50 d-track__start-column'>#</div>
+					<div className='text-white/50 d-track__name'>Name</div>
+					<div className='text-white/50 d-track__artist mr-[5px]'>Author</div>
+				</li>
+				{favorites.map((mus, index) => (
+					<CollectionTrackItem index={index} mus={mus} key={mus._id} />
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default Tracks
