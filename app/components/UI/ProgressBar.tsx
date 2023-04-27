0335d28585593f1cc16b09404192d84a30e42fd2
VLADISLAW9 <80681837+VLADISLAW9@users.noreply.github.com>
2023-04-27 20:36:06 +0700

    add progress-bar

---
+++ app/components/UI/ProgressBar.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const ProgressBar: FC = () => {
+	return (
+		<div className='progress-bar'>
+			<div className='spinner' />
+		</div>
+	)
+}
+
+export default ProgressBar
--- app/components/layout/Layout.tsx
+++ app/components/layout/Layout.tsx
@@ -1,5 +1,7 @@
 import { IMusic } from '@/app/types/IMusic'
-import { FC } from 'react'
+import { Router } from 'next/router'
+import { FC, useState } from 'react'
+import ProgressBar from '../UI/ProgressBar'
 import Footer from './footer/Footer'
 import Header from './header/Header'
 import styles from './Layout.module.scss'
@@ -10,11 +12,29 @@ interface ILayoutProps {
 }
 
 const Layout: FC<ILayoutProps> = ({ children, music }) => {
+	const [isLoading, setIsLoading] = useState(false)
+
+	Router.events.on('routeChangeStart', () => {
+		setTimeout(() => {
+			setIsLoading(true)
+		}, 300)
+	})
+
+	Router.events.on('routeChangeComplete', () => {
+		setIsLoading(false)
+	})
+
+	Router.events.on('routeChangeError', () => {
+		setTimeout(() => {
+			setIsLoading(true)
+		}, 300)
+	})
+
 	return (
 		<div className={styles.layout}>
 			<Header />
+			{isLoading && <ProgressBar />}
 			<main className='content'>{children}</main>
-
 			<Footer />
 		</div>
 	)
