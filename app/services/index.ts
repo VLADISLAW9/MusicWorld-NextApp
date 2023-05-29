1b5e0241870d7f91598cd656e4cf5be5f71644cd
VLADISLAW9 <80681837+VLADISLAW9@users.noreply.github.com>
2023-05-30 01:34:46 +0700

    some updates

--- app/services/index.ts
+++ app/services/index.ts
@@ -1,19 +1,21 @@
+const API_URL = 'http://localhost:3000/api'
+
 import { IAuthor } from '../types/IAuthor'
 import { IMusic } from '../types/IMusic'
 
 export async function getAllMusic(): Promise<IMusic[]> {
-	const response = await fetch(`${process.env.API_URL}/music`)
+	const response = await fetch(`${API_URL}/music`)
 	return await response.json()
 }
 
 export async function getAllAuthors(): Promise<IAuthor[]> {
-	const response = await fetch(`${process.env.API_URL}/authors`)
+	const response = await fetch(`${API_URL}/authors`)
 	return await response.json()
 }
 
 export async function getCurrentAuthor(
 	id: string | string[]
 ): Promise<IAuthor[]> {
-	const response = await fetch(`${process.env.API_URL}/authors/${id}`)
+	const response = await fetch(`${API_URL}/authors/${id}`)
 	return await response.json()
 }
