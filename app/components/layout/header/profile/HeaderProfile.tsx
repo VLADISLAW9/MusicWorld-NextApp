9f27c27888d7a475b02bae89a0399f654546cc08
VLADISLAW9 <80681837+VLADISLAW9@users.noreply.github.com>
2023-05-30 16:08:54 +0700

    bug fix

--- app/components/layout/header/profile/HeaderProfile.tsx
+++ app/components/layout/header/profile/HeaderProfile.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const HeaderProfile = () => {
 	return (
 		<div className='w-[30%] '>
-			<Link className='float-right' href='/profile'>
+			<Link className='float-right' href='/collection'>
 				<Avatar alt='user' src='' />
 			</Link>
 		</div>
